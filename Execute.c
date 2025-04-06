40c828b8fb01a916ec965aaece392331681c7044
SilverCoin00 <dang.nguyen.sp@gmail.com>
2025-04-06 15:17:34 +0700

    Update Execute.c

--- Execute.c
+++ Execute.c
@@ -1,7 +1,7 @@
 #include "Core.h"
 
 int main() {
-	char file[] = "D:\\Data\\archive1\\coffee_shop_revenue.csv";
+	char file[] = "coffee_shop_revenue.csv";
     Data_Frame* df = read_csv(file, 1000, ",");
     Dataset* ds = trans_dframe_to_dset(df, "Daily_Revenue");
     Standard_scaler* scaler = (Standard_scaler*)malloc(sizeof(Standard_scaler));
